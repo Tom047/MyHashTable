f2e40075cf0f4728e4b9998d6e06db0f4b130642
Murad <tangiev.murad@mail.ru>
2022-05-10 18:23:24 +0600

    Fixed hash-function and added constructors, get() and remove() functions

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class MyHashTable<K, V> {
 
     private class HashNode<K, V> {
@@ -16,29 +18,77 @@ public class MyHashTable<K, V> {
         }
     }
 
-    private Object[] chainArray;
+    private ArrayList<HashNode<K, V>> chainArray;
     private int M = 11;
     private int size;
 
     public MyHashTable() {
-        chainArray = new Object[M];
+        chainArray = new ArrayList<>(M);
+        size = 0;
+
+        for (int i = 0; i < M; i++)
+            chainArray.add(null);
     }
 
     public MyHashTable(int M) {
         this.M = M;
-        chainArray = new Object[this.M];
+        chainArray = new ArrayList<>(M);
+        size = 0;
+
+        for (int i = 0; i < M; i++)
+            chainArray.add(null);
     }
 
     private int hash(K key) {
-        int hash = java.lang.System.identityHashCode(key);
-        return (hash & 0x7fffffff) % M;
+        return (hashCode(key) & 0x7fffffff) % M;
+    }
+
+    private int hashCode(K key) {
+        return java.lang.System.identityHashCode(key);
     }
 
     public void put(K key, V value) {}
 
-    public V get(K key) {return null;}
+    public V get(K key) {
+        int index = hash(key);
+        while(chainArray.get(index) != null) {
 
-    public V remote(K key) {return null;}
+            if(chainArray.get(index).key == key)
+            return chainArray.get(index).value;
+
+            index++;
+
+            index %= M;
+        }
+
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> head = chainArray.get(0);
+        HashNode<K, V> prev = null;
+
+        while (head != null) {
+            if (head.key.equals(key) && hashCode(key) == hashCode(head.key))
+                break;
+
+            prev = head;
+            head = head.next;
+        }
+
+        if (head == null)
+            return null;
+
+        size--;
+
+        if (prev != null)
+            prev.next = head.next;
+        else
+            chainArray.set(index, head.next);
+
+        return head.value;
+    }
 
     public boolean contains(V value) {return false;}
 
