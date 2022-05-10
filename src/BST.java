c0df4be792e20fde7d25f9208c432377af2f7a38
Murad <tangiev.murad@mail.ru>
2022-05-10 20:56:00 +0600

    Added put() function and inorder traversal

--- src/BST.java
+++ src/BST.java
@@ -1,5 +1,9 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class BST<K extends Comparable<K>, V> {
     private Node root;
+
     private class Node {
         private K key;
         private V value;
@@ -10,11 +14,43 @@ public class BST<K extends Comparable<K>, V> {
         }
     }
 
-    public void put(K key, V value) {}
+    public BST() {
+        root = null;
+    }
+
+    public void put(K key, V value) {
+        Node n = new Node(key, value);
+        root = put(root, n);
+    }
+
+    public Node put(Node root, Node n) {
+        if (root == null) {
+            root = new Node(n.key, n.value);
+            return root;
+        }
+
+        if (n.key.compareTo(root.key) < 0)
+            root.left = put(root.left, n);
+        else if (n.key.compareTo(root.key) > 0)
+            root.right = put(root.right, n);
+
+        return root;
+    }
 
     public V get(K key) {return null;}
 
     public void delete(K key) {}
 
-    public Iterable<K> iterator() {return null;}
+    public Iterable<K> iterator() {
+        PriorityQueue<K> q = new PriorityQueue<K>();
+        inorder(root, q);
+        return q;
+    }
+
+    private void inorder(Node n, PriorityQueue<K> q) {
+        if(n == null) return;
+        inorder(n.left, q);
+        q.remove(n.key);
+        inorder(n.right, q);
+    }
 }
